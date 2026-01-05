e6628439c8cc5166c6124458af329428abb3c3c1
singhnilay <itsnilayyy@gmail.com>
2026-01-05 16:51:56 +0530

    Initial commit

---
+++ api/application.py
@@ -0,0 +1,39 @@
+from flask import Flask
+app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+
+
+class Drink(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(80), unique = True, nullable = False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self) : 
+        return f"{self.name} - {self.description}"
+
+
+@app.route('/')
+def index():
+    return 'Hello'
+
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+
+    output = []
+
+    for drink in drinks:
+        drink_data = {'name' : drink.name, 'description' : drink.description}
+        output.append(drink_data)
+
+    return {"drinks" : output}
+
+@app.route('/drinks/<id>')
+def get_drink(id):
+    drink = Drink.query.get_or_404(id)
+    return ({'name' : drink.name, 'description' : drink.description})
